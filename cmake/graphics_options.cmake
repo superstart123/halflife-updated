18df15e81d3d42d87da9a5e0d31f8357c7b99724
superstart123 <superstart1233@proton.me>
2026-06-04 23:27:27 +0300

    Add graphics enhancement build options

---
+++ cmake/graphics_options.cmake
@@ -0,0 +1,3 @@
+option(HalfLife_LTO "Enable Link-Time Optimization" ON)
+option(HalfLife_ENABLE_GRAPHICS_ENHANCEMENTS "Enable Graphics Enhancements" ON)
+option(HalfLife_ENABLE_PERFORMANCE_OPTIMIZATIONS "Enable Performance Optimizations" ON)
